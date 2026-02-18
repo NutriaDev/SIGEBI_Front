1498bdf33448fb240016abd0308c6e4566eb97d3
DianaArevalo <morec9834@gmail.com>
2026-02-17 23:11:58 -0500

    responsive dashboard

--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -7,7 +7,12 @@ import { AuthService } from '../../core/services/auth.service';
   styleUrls: ['./main-layout.component.css'],
 })
 export class MainLayoutComponent {
-  constructor(public authService: AuthService) {} 
+  constructor(public authService: AuthService) {}
+  isSidebarOpen = false;
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 
   has(permission: string): boolean {
     return this.authService.hasPermission(permission);
