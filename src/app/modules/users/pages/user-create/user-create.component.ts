419e46849d9992e77873a851db030679bea0396f
DianaArevalo <morec9834@gmail.com>
2026-02-18 21:29:28 -0500

    create-user style

--- src/app/modules/users/pages/user-create/user-create.component.ts
+++ src/app/modules/users/pages/user-create/user-create.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-create',
   templateUrl: './user-create.component.html',
-  styleUrl: './user-create.component.css'
 })
 export class UserCreateComponent {
+  userForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      role: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      birthDate: [''],
+      phone: [''],
+      document: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      entity: [''],
+      password: ['', Validators.required],
+    });
+  }
+
+  submit() {
+    if (this.userForm.invalid) {
+      this.userForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.userForm.value); // luego lo conectamos al backend
+  }
 }
