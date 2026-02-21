85a58f76c07e08f1e04ab043dfe29407eddec50a
DianaArevalo <morec9834@gmail.com>
2026-02-20 21:56:15 -0500

    get-all list styles

--- src/app/modules/users/pages/user-list/user-list.component.ts
+++ src/app/modules/users/pages/user-list/user-list.component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../../core/services/auth.service';
+import { TabService } from '../../../dashboard/services/tab.service';
+import { UserEditComponent } from '../user-edit/user-edit.component';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
 })
-export class UserListComponent {}
+export class UserListComponent {
+  constructor(
+    private userService: AuthService,
+    private tabService: TabService,
+  ) {}
+  users: any;
+
+  openEditUser() {
+    this.tabService.openTab('Editar Usuario', UserEditComponent);
+  }
+}
