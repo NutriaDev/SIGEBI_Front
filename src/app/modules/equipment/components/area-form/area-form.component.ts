85db0d337dbe58ac102b7046982e785052dbcbbd
DianaArevalo <morec9834@gmail.com>
2026-03-10 12:24:52 -0500

    initial commit equipments

---
+++ src/app/modules/equipment/components/area-form/area-form.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-area-form',
+  templateUrl: './area-form.component.html',
+})
+export class AreaFormComponent {}
---
+++ src/app/modules/equipment/components/clasification-form/clasification-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clasification-form',
+  templateUrl: './clasification-form.component.html',
+  styleUrl: './clasification-form.component.css'
+})
+export class ClasificationFormComponent {
+
+}
