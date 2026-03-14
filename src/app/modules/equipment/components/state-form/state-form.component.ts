219b1cccec417c4522015a7755e339fc7e38e3b4
DianaArevalo <morec9834@gmail.com>
2026-03-13 19:01:38 -0500

    style states

--- src/app/modules/equipment/components/state-form/state-form.component.ts
+++ src/app/modules/equipment/components/state-form/state-form.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { StateService } from '../../services/state.service';
 
 @Component({
   selector: 'app-state-form',
   templateUrl: './state-form.component.html',
-  styleUrl: './state-form.component.css'
 })
 export class StateFormComponent {
-
+  constructor(public service: StateService) {}
 }
--- src/app/modules/equipment/services/state.service.ts
+++ src/app/modules/equipment/services/state.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BaseEntity } from 'app/core/models/base-equipments-entity';
+import { EquipmentService } from 'app/core/services/equipment.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class StateService {
-
-  constructor() { }
+export class StateService extends EquipmentService<BaseEntity> {
+  constructor(http: HttpClient) {
+    super(http, 'http://localhost:8080/api/states');
+  }
 }
