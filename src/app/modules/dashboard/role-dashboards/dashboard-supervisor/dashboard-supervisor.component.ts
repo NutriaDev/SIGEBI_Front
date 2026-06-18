e395a61550956b9477736db3d57b79936e37ac28
DianaArevalo <morec9834@gmail.com>
2026-06-18 06:27:48 -0500

    btn reports

--- src/app/modules/dashboard/role-dashboards/dashboard-supervisor/dashboard-supervisor.component.ts
+++ src/app/modules/dashboard/role-dashboards/dashboard-supervisor/dashboard-supervisor.component.ts
@@ -3,6 +3,7 @@ import { TabService } from '../../services/tab.service';
 import { UserListComponent } from '../../../users/pages/user-list/user-list.component';
 import { ReportsModule } from '../../../reports/reports.module';
 import { EquipmentLifecycleComponent } from 'app/modules/equipment/pages/equipment-lifecycle/equipment-lifecycle.component';
+import { ConsolidatedReportComponent } from 'app/modules/reports/pages/consolidated-report/consolidated-report.component';
 
 @Component({
   selector: 'app-dashboard-supervisor',
@@ -10,14 +11,16 @@ import { EquipmentLifecycleComponent } from 'app/modules/equipment/pages/equipme
 })
 export class DashboardSupervisorComponent {
   constructor(private tabService: TabService) {}
+  isSidebarOpen = false;
 
   openUsers() {
     this.tabService.openTab('Usuarios', UserListComponent);
   }
 
-  openReports() {
-    this.tabService.openTab('Reportes', ReportsModule);
-  }
+    consolidatedReport() {
+        this.tabService.openTab(
+          'Reporte Consolidado', ConsolidatedReportComponent);
+        this.isSidebarOpen = false;}
 
   openEquipmentLifecycle() {
     this.tabService.openTab(
